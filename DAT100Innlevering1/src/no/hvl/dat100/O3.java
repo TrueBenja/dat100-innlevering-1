b2a760aa9e8ef069f932ac0411ac1ead43d43869
TrueBenja <benjaminstoreboe@gmail.com>
2024-09-03 18:53:26 +0200

    Oppdatert O3 med brukerinput

--- DAT100Innlevering1/src/no/hvl/dat100/O3.java
+++ DAT100Innlevering1/src/no/hvl/dat100/O3.java
@@ -1,11 +1,25 @@
 package no.hvl.dat100;
 
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
 public class O3 {
     public static void main(String[] args) {
-        System.out.println(fakultet(4));
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Skriv inn en verdi større enn 0: ");
+        int num = parseInt(input.nextLine());
+
+        while (num < 0) {
+            System.out.print("Skriv inn en verdi større enn 0: ");
+            num = parseInt(input.nextLine());
+        }
+
+        System.out.println(fakultet(num));
     }
 
     private static int fakultet(int n) {
+
         int sum = 1;
 
         if (n > 0) {
