dac6a1155362161783215895fc3197e4f2ee52d5
TrueBenja <benjaminstoreboe@gmail.com>
2024-09-03 18:23:25 +0200

    Oppgave 1 og 2 gjort

---
+++ DAT100Innlevering1/src/no/hvl/dat100/O1.java
@@ -0,0 +1,75 @@
+package no.hvl.dat100;
+
+import java.util.Scanner;
+import static java.lang.Math.round;
+
+public class O1 {
+    public static void main(String[] args) {
+        System.out.println("Trinnskatt: " + beregnTrinnskatt());
+    }
+
+    private static double beregnTrinnskatt() {
+        int trinn0 = 208050;
+        int trinn1 = 292850;
+        int trinn2 = 670000;
+        int trinn3 = 937900;
+        int trinn4 = 1350000;
+
+        double trinnskatt1 = 1.7 / 100;
+        double trinnskatt2 = 4.0 / 100;
+        double trinnskatt3 = 13.6 / 100;
+        double trinnskatt4 = 16.6 / 100;
+        double trinnskatt5 = 17.6 / 100;
+
+        Scanner input = new Scanner(System.in);
+
+        double lonn = input.nextInt();
+
+        double totalTrinnskatt = 0.0;
+
+        if (lonn <= trinn0) {
+            return 0.0;
+        }
+
+        double interim;
+
+        if (lonn <= trinn1) {
+            interim = lonn - trinn0;
+            totalTrinnskatt += interim * trinnskatt1;
+        } else if (lonn <= trinn2) {
+            interim = trinn1 - trinn0;
+            totalTrinnskatt += interim * trinnskatt1;
+            interim = lonn - trinn1;
+            totalTrinnskatt += interim * trinnskatt2;
+        } else if (lonn <= trinn3) {
+            interim = trinn1 - trinn0;
+            totalTrinnskatt += interim * trinnskatt1;
+            interim = trinn2 - trinn1;
+            totalTrinnskatt += interim * trinnskatt2;
+            interim = lonn - trinn2;
+            totalTrinnskatt += interim * trinnskatt3;
+        } else if (lonn <= trinn4) {
+            interim = trinn1 - trinn0;
+            totalTrinnskatt += interim * trinnskatt1;
+            interim = trinn2 - trinn1;
+            totalTrinnskatt += interim * trinnskatt2;
+            interim = trinn3 - trinn2;
+            totalTrinnskatt += interim * trinnskatt3;
+            interim = lonn - trinn3;
+            totalTrinnskatt += interim * trinnskatt4;
+        } else {
+            interim = trinn1 - trinn0;
+            totalTrinnskatt += interim * trinnskatt1;
+            interim = trinn2 - trinn1;
+            totalTrinnskatt += interim * trinnskatt2;
+            interim = trinn3 - trinn2;
+            totalTrinnskatt += interim * trinnskatt3;
+            interim = trinn4 - trinn3;
+            totalTrinnskatt += interim * trinnskatt4;
+            interim = lonn - trinn4;
+            totalTrinnskatt += interim * trinnskatt5;
+        }
+        totalTrinnskatt = round(totalTrinnskatt);
+        return totalTrinnskatt;
+    }
+}
---
+++ DAT100Innlevering1/src/no/hvl/dat100/O2.java
@@ -0,0 +1,35 @@
+package no.hvl.dat100;
+
+import java.util.Scanner;
+
+public class O2 {
+    public static void main(String[] args) {
+        int antStud = 10;
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < antStud; i++) {
+            System.out.print("Poengsum: ");
+            int poengsum = input.nextInt();
+
+            while (poengsum > 100 || poengsum < 0) {
+                System.out.print("Ugyldig poengsum. Prøv igjen: ");
+                poengsum = input.nextInt();
+            }
+
+            if (poengsum >= 90) {
+                System.out.println("Poengsum: " + poengsum + ", Karakter: A");
+            } else if (poengsum >= 80) {
+                System.out.println("Poengsum: " + poengsum + ", Karakter: B");
+            } else if (poengsum >= 60) {
+                System.out.println("Poengsum: " + poengsum + ", Karakter: C");
+            } else if (poengsum >= 50) {
+                System.out.println("Poengsum: " + poengsum + ", Karakter: D");
+            } else if (poengsum >= 40) {
+                System.out.println("Poengsum: " + poengsum + ", Karakter: E");
+            } else {
+                System.out.println("Poengsum: " + poengsum + ", Karakter: F");
+            }
+        }
+    }
+}
