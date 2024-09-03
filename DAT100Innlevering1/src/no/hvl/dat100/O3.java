0388aaa06e5bad99252a2608502bc9bda03d5403
TrueBenja <benjaminstoreboe@gmail.com>
2024-09-03 18:48:03 +0200

    Gjort oppgave 3

--- DAT100Innlevering1/src/no/hvl/dat100/O3.java
+++ DAT100Innlevering1/src/no/hvl/dat100/O3.java
@@ -1,5 +1,17 @@
 package no.hvl.dat100;
 
 public class O3 {
+    public static void main(String[] args) {
+        System.out.println(fakultet(4));
+    }
 
+    private static int fakultet(int n) {
+        int sum = 1;
+
+        if (n > 0) {
+            sum = n * fakultet(n - 1);
+        }
+
+        return sum;
+    }
 }
